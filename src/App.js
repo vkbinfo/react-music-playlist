2047cdd28e5fc7ada88994c222e27fdaa87074ad
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2018-07-03 11:34:54 +0530

    now playlist is loading in start without the filter

--- src/App.js
+++ src/App.js
@@ -5,46 +5,11 @@ let colorSetting = {
  color : '#fdg'
 };
 
-let fakeServerData = {
-  user : {
-    name : "David",
-    playlists : [
-                { name: "Pink Floyd's Best",
-                  songs: [
-                    {name: "Shine on crazy Diamond", length: 345},
-                    {name: "Wish You were here", length: 800},
-                    {name: "Us and Them", length: 900}
-                  ]
-                },
-                {name: "The Endless River",
-                songs: [
-                  {name: "Wake Up", length: 345},
-                  {name: "Wish You were here", length: 800},
-                  {name: "Us and Them", length: 900}
-                ]
-                },
-                {name: "Wish you were here",
-                songs: [
-                  {name: "Time", length: 345},
-                  {name: "Wish You were here", length: 800},
-                  {name: "Us and Them", length: 900}
-                ]
-                },
-                {name: "The dark side of the Moon.",
-                songs: [
-                  {name: "High Feels", length: 345},
-                  {name: "Wish You were here", length: 800},
-                  {name: "Us and Them", length: 900}
-                ]
-              }
-            ]
-  }
-}
 class PlaylistCounter extends Component {
   render(){
     return (
       <div style={{...colorSetting, width: "40%", display: 'inline-block'}}>
-        <h2>{this.props.playlists.length} Playlists</h2>
+        <h2>{this.props.userPlaylists.length} Playlists</h2>
       </div>
     );
   }
@@ -52,25 +17,14 @@ class PlaylistCounter extends Component {
 
 class HoursCounter extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {};
-  }
-
-  componentWillReceiveProps(nextProps){
-    // updated props will come here.
-    this.setState({playlists:nextProps.playlists})
-    this.render()
-  }
-
   render(){
-    let songsToLoad = this.state.playlists
-    ?(this.state.playlists.reduce((allPlaylist, playlist) =>{
+    let songsToLoad = this.props.userPlaylists
+    ?(this.props.userPlaylists.reduce((allPlaylist, playlist) =>{
       return allPlaylist.concat(playlist.songs)}
       ,[]))
     :[]
-    let totalDuaration = songsToLoad.reduce((sum, song) => sum + song.length, 0);
-
+   let totalDuaration = songsToLoad.reduce((sum, song) => ((song === undefined)?sum + 0:sum + song.length), 0)
+  
     return (
       <div style={{...colorSetting, width: "40%", display: 'inline-block'}}>
         <h2>{Math.round(totalDuaration/60)} Minutes</h2>
@@ -98,7 +52,8 @@ class Playlist extends Component {
       <img src={this.props.playlist.imageURL} style={{width:"120px"}}/>
       <h3>{this.props.playlist.name}</h3>
       <ul style = { {float: "left"} }>
-      {this.props.playlist.songs.map(song => <li>{song.name}</li>)}
+      { this.props.playlist.songs 
+        && this.props.playlist.songs.map(song => <li>{song.name}</li>)}
       </ul>
       </div>
     );
@@ -114,14 +69,16 @@ class App extends Component {
     };
   }
 
-   getSongs(tracksInfo, accessToken){
-    return fetch(tracksInfo.href,{headers: {Authorization: 'Bearer ' + accessToken}})
+   async getSongs(tracksInfo, accessToken){
+    let response = await fetch(tracksInfo.href,{headers: {Authorization: 'Bearer ' + accessToken}})
                           .then(  response => response.json())
-                          .then(data => data.items.map(track =>{
-                                                          let singleTrackInfo = {name:track.track.name, length:330}
+                          .then(data => data.items.map(songObj =>{
+                                                          let track =songObj.track
+                                                          let singleTrackInfo = {name:track.name, length:Math.round(track.duration_ms/1000)}
                                                           return singleTrackInfo;
                                                           })
-                                )                        
+                                )
+    return response
   }
 
   componentDidMount(){
@@ -142,33 +99,50 @@ class App extends Component {
      //let's get playlists of user
       fetch("https://api.spotify.com/v1/me/playlists", {headers: {Authorization: 'Bearer ' + accessToken}})
       .then( response => response.json() )
-      .then( data=>this.setState({playlists: data.items.map( playlist => { 
+      .then( data=>data.items.map( async playlist => { 
                                                       let playlistDetail={}
                                                       playlistDetail['name'] = playlist.name;
                                                       playlistDetail['imageURL']= playlist.images[0].url
-                                                      this.getSongs(playlist.tracks,accessToken).then(data=>playlistDetail['songs']=data)
+                                                      playlistDetail['songs'] = await this.getSongs(playlist.tracks,accessToken)
                                                       return playlistDetail
                                                     } 
                                       )
-                                  })
             )
-   
-    
+      // We should get all ansynchronmous data then we should set the state, 
+      // otherwise object will be created in state and something gets updates in object in deeper
+      // level the setState will not get applied. Allways apply setState when you have full data.
+      .then(data => {
+        data.map(playlistPromise=> playlistPromise.then(playlist=>
+          {
+            let playlistsNow = this.state.playlists
+                                ?this.state.playlists
+                                :[];
+            playlistsNow.push(playlist)
+            this.setState({playlists:this.state.playlists
+                          ? playlistsNow
+                          :[]
+                        })
+          }))
+    })
   }
 
+  
   render() {
-    let playListToRender = this.state.playlists
+    let playListToRender = this.state.playlists && this.state.user
     ? this.state.playlists.filter( playlist =>
-      playlist.name.toLowerCase().includes(this.state.filter.toLowerCase())) 
+     {
+      return playlist.name.toLowerCase().includes(this.state.filter.toLowerCase())
+      }) 
     : []
+
     return (
       <div className = "App">
         {
           this.state.user 
           ?<div>
             <h1 style = {{...colorSetting, fontSize : '54px'}}> {this.state.user.name}'s Playlist </h1>
-            <PlaylistCounter playlists = {playListToRender} /> 
-            <HoursCounter playlists = {playListToRender} />
+            <PlaylistCounter userPlaylists = {playListToRender} /> 
+            <HoursCounter userPlaylists = {playListToRender} />
             <Filter onTextChange={
               filterString => 
               this.setState({filter:filterString})
